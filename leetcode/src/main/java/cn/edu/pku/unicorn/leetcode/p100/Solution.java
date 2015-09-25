7cb932f5ef07781fd1fd1894aa63aed3888f2017
hqs0417 <zhengkun1992@126.com>
2015-09-26 00:47:40 +0800

    100-Same Tree

---
+++ leetcode/src/main/java/cn/edu/pku/unicorn/leetcode/p100/Solution.java
@@ -0,0 +1,33 @@
+package cn.edu.pku.unicorn.leetcode.p100;
+
+import cn.edu.pku.unicorn.leetcode.common.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * Created by unicorn on 15-9-26.<br/>
+ * p100 Same Tree <br/>
+ * Given two binary trees, write a function to check if they are equal or not.<br/>
+ * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.<br/>
+ *
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if( p == null && q == null ) {
+            return true;
+        }
+        if(p == null && q != null || p != null && q == null) {
+            return false;
+        }
+        if( p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode p = new TreeNode(1);
+        TreeNode q = null;
+        System.out.println(solution.isSameTree(p, q));
+    }
+}
