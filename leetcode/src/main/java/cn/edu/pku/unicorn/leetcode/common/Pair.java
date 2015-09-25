09983ab0c4ef82c2369a276ad5741feb797a4dda
hqs0417 <zhengkun1992@126.com>
2015-09-25 22:09:35 +0800

    为Pair增加泛型支持

--- leetcode/src/main/java/cn/edu/pku/unicorn/leetcode/common/Pair.java
+++ leetcode/src/main/java/cn/edu/pku/unicorn/leetcode/common/Pair.java
@@ -1,12 +1,29 @@
 package cn.edu.pku.unicorn.leetcode.common;
 
-public class Pair {
-	int x, y;
+public class Pair<X, Y> {
+	private X x;
+    private Y y;
 	public Pair(){
 		
 	}
-	public Pair(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Pair(X x, Y y) {
+        this.x = x;
+        this.y = y;
 	}
+
+    public X getX() {
+        return x;
+    }
+
+    public void setX(X x) {
+        this.x = x;
+    }
+
+    public Y getY() {
+        return y;
+    }
+
+    public void setY(Y y) {
+        this.y = y;
+    }
 }
