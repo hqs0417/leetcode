1439f0b714d67b3e5dc1905e7e8ff38fda586ca9
hqs0417 <zhengkun1992@126.com>
2015-09-27 20:43:52 +0800

    p027

---
+++ leetcode/src/main/java/cn/edu/pku/unicorn/leetcode/p027/Solution.java
@@ -0,0 +1,24 @@
+package cn.edu.pku.unicorn.leetcode.p027;
+
+/**
+ * Created by Unicorn on 2015/9/27.
+ */
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int p = -1;
+        for(int i = 0;i < nums.length; i++) {
+            if(p == -1) {
+                if(nums[i] == val) {
+                    p = i;
+                }
+            } else {
+                if(nums[i] != val) {
+                    nums[p] = nums[i];
+                    nums[i] = val;
+                    p++;
+                }
+            }
+        }
+        return p == -1? nums.length : p;
+    }
+}
