da2495106040e0606a34f6453bfdb9fbec075add
Kun Zheng <zhengkun1992@126.com>
2016-01-03 21:12:58 +0800

    292 Nim Game

---
+++ leetcode/src/main/java/cn/edu/pku/unicorn/leetcode/p292/Solution.java
@@ -0,0 +1,11 @@
+package cn.edu.pku.unicorn.leetcode.p292;
+
+/**
+ * Created by Kun Zheng on 2016/1/3.
+ * #293 Nim Game
+ */
+public class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
