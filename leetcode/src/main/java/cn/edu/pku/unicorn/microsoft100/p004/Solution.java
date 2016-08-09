dec160642125746f521a3d44715a1272f01f4acd
Kun Zheng <v_zhengkun@baidu.com>
2016-08-09 21:26:11 +0800

    2016-08-09 添加微软算法100题

--- leetcode/src/main/java/cn/edu/pku/unicorn/microsoft100/p004/Solution.java
+++ leetcode/src/main/java/cn/edu/pku/unicorn/microsoft100/p004/Solution.java
@@ -11,11 +11,24 @@ import cn.edu.pku.unicorn.microsoft100.common.TreeNode;
  */
 public class Solution {
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n12 = new TreeNode(12);
+        root.left = n5;
+        root.right = n12;
 
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n7 = new TreeNode(7);
+        n5.left = n3;
+        n5.right = n7;
+
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.binaryTreeSumPath(root, 22);
+        System.out.println(result);
     }
 
     /**
-     * 深度搜索
+     * 深度搜索 回溯 递归
      * @param root
      * @param target
      * @return
@@ -30,13 +43,18 @@ public class Solution {
         if (root == null) {
             return;
         }
-        // TODO 注意可能会重复添加
-        if (target == 0) {
-            result.add(new ArrayList<>(path));
-        }
         path.add(root.val);
-        target = target - root.val;
-        binaryTreeSumPathHelper(root.left, result, path, target);
-        binaryTreeSumPathHelper(root.right, result, path, target);
+
+        if (root.val == target) {
+            result.add(new ArrayList<>(path));
+        } else {
+            if (root.left != null) {
+                binaryTreeSumPathHelper(root.left, result, path, target - root.val);
+            }
+            if (root.right != null) {
+                binaryTreeSumPathHelper(root.right, result, path, target - root.val);
+            }
+        }
+        path.remove(path.size() - 1);
     }
 }
